533cb94dba722005db7b320b9e4574279bbcebae
hninwutyikyaw <hninwutyikyaw.26@gmail.com>
2023-02-20 01:29:56 +0630

    first commit

---
+++ resources/views/blogs.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+    <article>
+        <h1>
+            <a href="/blog">First Blog</a>
+        </h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam ex voluptas sint soluta minus harum sunt nemo. Repellendus delectus eveniet itaque aperiam harum fugit ipsum quo sint expedita maxime ad labore modi sequi doloremque corrupti soluta maiores suscipit culpa, illum odit mollitia. Nesciunt repudiandae a nam quo ipsam distinctio consectetur cupiditate veniam, corrupti accusantium? Quasi obcaecati animi culpa eligendi architecto natus, molestias dolor ab eum at doloremque suscipit, deserunt, repellendus asperiores dolores quod. Fuga architecto, maiores mollitia praesentium unde odit explicabo itaque expedita, voluptates excepturi quod voluptatem eveniet quibusdam nulla corporis vero corrupti dolor! Quidem, ipsum. Dolorum natus soluta officia iste aperiam, velit eos debitis earum aliquam rerum neque sed, quasi libero veritatis saepe? Corporis impedit quaerat minus, sapiente veniam consequuntur asperiores assumenda saepe magnam dignissimos ex quod quasi nemo similique veritatis magni molestias quam libero eum amet accusamus officiis esse! Molestiae omnis laboriosam harum id perferendis. Cumque, error asperiores quas sunt expedita natus eveniet! Libero dolore ullam accusantium perferendis reprehenderit, odio deserunt. Necessitatibus, molestias obcaecati! Vel rem harum sit dignissimos corporis. Ea neque consequuntur reprehenderit excepturi aspernatur non sint nostrum error, dignissimos optio fuga modi necessitatibus molestias laborum quis delectus recusandae cupiditate aliquid eaque distinctio, similique ipsam, saepe adipisci!</p>
+    </article>
+    <article>
+        <h1>
+            <a href="/blog">Second Blog</a>
+        </h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam ex voluptas sint soluta minus harum sunt nemo. Repellendus delectus eveniet itaque aperiam harum fugit ipsum quo sint expedita maxime ad labore modi sequi doloremque corrupti soluta maiores suscipit culpa, illum odit mollitia. Nesciunt repudiandae a nam quo ipsam distinctio consectetur cupiditate veniam, corrupti accusantium? Quasi obcaecati animi culpa eligendi architecto natus, molestias dolor ab eum at doloremque suscipit, deserunt, repellendus asperiores dolores quod. Fuga architecto, maiores mollitia praesentium unde odit explicabo itaque expedita, voluptates excepturi quod voluptatem eveniet quibusdam nulla corporis vero corrupti dolor! Quidem, ipsum. Dolorum natus soluta officia iste aperiam, velit eos debitis earum aliquam rerum neque sed, quasi libero veritatis saepe? Corporis impedit quaerat minus, sapiente veniam consequuntur asperiores assumenda saepe magnam dignissimos ex quod quasi nemo similique veritatis magni molestias quam libero eum amet accusamus officiis esse! Molestiae omnis laboriosam harum id perferendis. Cumque, error asperiores quas sunt expedita natus eveniet! Libero dolore ullam accusantium perferendis reprehenderit, odio deserunt. Necessitatibus, molestias obcaecati! Vel rem harum sit dignissimos corporis. Ea neque consequuntur reprehenderit excepturi aspernatur non sint nostrum error, dignissimos optio fuga modi necessitatibus molestias laborum quis delectus recusandae cupiditate aliquid eaque distinctio, similique ipsam, saepe adipisci!</p>
+    </article>
+    <article>
+        <h1>
+            <a href="/blog">Third Blog</a>
+        </h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam ex voluptas sint soluta minus harum sunt nemo. Repellendus delectus eveniet itaque aperiam harum fugit ipsum quo sint expedita maxime ad labore modi sequi doloremque corrupti soluta maiores suscipit culpa, illum odit mollitia. Nesciunt repudiandae a nam quo ipsam distinctio consectetur cupiditate veniam, corrupti accusantium? Quasi obcaecati animi culpa eligendi architecto natus, molestias dolor ab eum at doloremque suscipit, deserunt, repellendus asperiores dolores quod. Fuga architecto, maiores mollitia praesentium unde odit explicabo itaque expedita, voluptates excepturi quod voluptatem eveniet quibusdam nulla corporis vero corrupti dolor! Quidem, ipsum. Dolorum natus soluta officia iste aperiam, velit eos debitis earum aliquam rerum neque sed, quasi libero veritatis saepe? Corporis impedit quaerat minus, sapiente veniam consequuntur asperiores assumenda saepe magnam dignissimos ex quod quasi nemo similique veritatis magni molestias quam libero eum amet accusamus officiis esse! Molestiae omnis laboriosam harum id perferendis. Cumque, error asperiores quas sunt expedita natus eveniet! Libero dolore ullam accusantium perferendis reprehenderit, odio deserunt. Necessitatibus, molestias obcaecati! Vel rem harum sit dignissimos corporis. Ea neque consequuntur reprehenderit excepturi aspernatur non sint nostrum error, dignissimos optio fuga modi necessitatibus molestias laborum quis delectus recusandae cupiditate aliquid eaque distinctio, similique ipsam, saepe adipisci!</p>
+    </article>
+</body>
+</html>
